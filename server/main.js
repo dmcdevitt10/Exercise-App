9790f9ec48aa10e995af57e292043e20a40b369f
Daniel McDevitt <danielmcdevitt10@gmail.com>
2023-05-15 18:47:07 -0600

    created workout and sets_reps tables and established relationships to the three existing tables

--- server/main.js
+++ server/main.js
@@ -4,11 +4,17 @@ const express = require("express");
 const cors = require("cors");
 const { PORT } = process.env;
 const { database } = require("./util/database");
-const {User} = require('./util/models')
+const {User, Workout, setsReps} = require('./util/models')
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+User.hasMany(Workout)
+Workout.belongsTo(User)
+
+Workout.hasOne(setsReps)
+setsReps.belongsTo(Workout)
 
 database.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
--- server/util/models.js
+++ server/util/models.js
@@ -18,4 +18,56 @@ module.exports = {
       allowNull: false,
     },
   }),
+  Workout: database.define("workout", {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    workout_name: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: false,
+    },
+    exercise1: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: false,
+    },
+    exercise2: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: true,
+    },
+    exercise3: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: true,
+    },
+    exercise4: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: true,
+    },
+    exercise5: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: true,
+    },
+    exercise6: {
+      type: DataTypes.STRING({ length: 50 }),
+      allowNull: true,
+    },
+  }),
+  setsReps: database.define("sets_reps", {
+    id: {
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    sets: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    reps: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }),
 };
